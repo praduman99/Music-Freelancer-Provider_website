1351c839f75aa9b5578d3bf37c6216aa2568cde9
praduman99 <pradumanvats.000@gmail.com>
2021-06-04 23:44:08 +0530

    Tenth day Commit

--- src/Pages/FreelancerProfile.js
+++ src/Pages/FreelancerProfile.js
@@ -1,7 +1,7 @@
 import { List, Row, Col, Image, Carousel } from 'antd'
 import React, { useEffect, useState } from 'react'
 import './style.css'
-import { MessageOutlined, PhoneOutlined ,  CheckOutlined } from '@ant-design/icons'
+import { MessageOutlined, PhoneOutlined, CheckOutlined } from '@ant-design/icons'
 import CardsRow from '../ReusableComponents/CardsRow';
 import Layout from 'antd/lib/layout/layout';
 const data = [
@@ -50,89 +50,90 @@ const FreelancerProfile = () => {
 
     return (
         <Layout>
-        <div style={{ marginTop: "3rem" }}>
-            <Row justify="center">
-                <div>
-                    <Image className="Profile_Image" src="https://images.unsplash.com/photo-1622398565884-861701ad4c0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" width={windowDimensions.width > 720 ? "350px" : windowDimensions.width + "px"} height="350px" />
-                </div>
-                <Col span={16} className="Heading_Data_Profile">
-                    <h1 className="Heading_FreelancerProfile">Ankel sin </h1>
-                    <Row justify="center"><h3> will mix and master your song to industry standard</h3></Row>
-                    <Row justify='center' gutter={20}>
-                        <div className="Heading_Text_Profile">Rating⭐⭐⭐⭐⭐(4.5) </div>
-                        <div className="Heading_Text_Profile"> Projects in queue(4)</div>
-                    </Row>
-                    <Row justify="center">
-                        <h3 style={{ fontFamily: "monospace" }}>Services</h3>
-                    </Row>
-                    <Row justify="center" >
-                        <Col span={12}>
-                            <List
-                                size="small"
+            <div style={{ marginTop: "3rem" }}>
+                <Row justify="center">
+                    <div>
+                        <Image className="Profile_Image" src="https://images.unsplash.com/photo-1622398565884-861701ad4c0a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" width={windowDimensions.width > 720 ? "350px" : windowDimensions.width + "px"} height="350px" />
+                    </div>
+                    <Col span={16} className="Heading_Data_Profile">
+                        <h1 className="Heading_FreelancerProfile">Ankel sin </h1>
+                        <Row justify="center"><h3> will mix and master your song to industry standard</h3></Row>
+                        <Row justify='center' gutter={20}>
+                            <div className="Heading_Text_Profile">Rating⭐⭐⭐⭐⭐(4.5) </div>
+                            <div className="Heading_Text_Profile"> Projects in queue(4)</div>
+                        </Row>
+                        <Row justify="center">
+                            <h3 style={{ fontFamily: "monospace" }}>Services</h3>
+                        </Row>
+                        <Row justify="center" >
+                            <Col span={12}>
+                                <List
+                                    size="small"
 
-                                dataSource={data}
-                                renderItem={item => <List.Item>{item}</List.Item>}
-                            />
-                        </Col>
-                        <Col span={8}><Row justify="center" > <h1>₹148 <span className="Price_Profile">/Song</span></h1> </Row></Col>
-                    </Row>
+                                    dataSource={data}
+                                    renderItem={item => <List.Item>{item}</List.Item>}
+                                />
+                            </Col>
+                            <Col span={8}><Row justify="center" > <h1>₹148 <span className="Price_Profile">/Song</span></h1> </Row></Col>
+                        </Row>
 
-                    <Row justify="center">
-                        <Col span={windowDimensions.width > 720 ? 8 : 12} >
-                            <button className="Heading_Button">
-                                <div>
-                                    <MessageOutlined />
-                                    <span>Message</span>
-                                </div>
-                            </button>
-                        </Col>
-                        {/* <Col span={windowDimensions.width > 720 ? 8 : 12}>
+                        <Row justify="center">
+                            <Col span={windowDimensions.width > 720 ? 8 : 12} >
+                                <button className="Heading_Button">
+                                    <div>
+                                        <MessageOutlined />
+                                        <span>Message</span>
+                                    </div>
+                                </button>
+                            </Col>
+                            {/* <Col span={windowDimensions.width > 720 ? 8 : 12}>
                             <button className="Heading_Button">
                                <span><PhoneOutlined /></span>
                                Call
                             </button>
                         </Col> */}
-                    </Row>
-                </Col>
-            </Row>
-            <Row  style={{padding:"1rem"}}>
-                <Col span={windowDimensions.width>720?16:24} >
-                    <Carousel afterChange={onChange} >
-                        <div>
-                            <h3 style={contentStyle}>1</h3>
-                        </div>
-                        <div>
-                            <h3 style={contentStyle}>2</h3>
-                        </div>
-                        <div>
-                            <h3 style={contentStyle}>3</h3>
-                        </div>
-                        <div>
-                            <h3 style={contentStyle}>4</h3>
-                        </div>
-                    </Carousel>
-                </Col>
-               
-                <List
-                    header={<div><h1 className="Price_card_heading"> Check Out Plan Here!</h1></div>}
-                    footer={<div style={{textAlign:'center'}}><button className="Heading_Button" style={{width:"100%"}}>Continue(₹143)</button></div>}
-                    bordered 
-                    dataSource={data1}
-                    style={{width:windowDimensions.width>720?"30%":"100%"}}
-                    renderItem={item => (
-                        <List.Item>
-                        
-                         <CheckOutlined style={{color:"darkorange",fontSize:"1rem"}} />  {item}
-                        </List.Item>
-                    )}
-                />
-            </Row>
-              <Row>
-                  <h1 className="Content_Heading">Some More Freelancers</h1>
-              </Row>
-                <CardsRow/>
-              
-        </div>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row style={{ padding: "1rem" }}>
+                    <Col span={windowDimensions.width > 720 ? 16 : 24} >
+                        <Carousel afterChange={onChange} >
+                            <div>
+                                <h3 style={contentStyle}>1</h3>
+                            </div>
+                            <div>
+                                <h3 style={contentStyle}>2</h3>
+                            </div>
+                            <div>
+                                <h3 style={contentStyle}>3</h3>
+                            </div>
+                            <div>
+                                <h3 style={contentStyle}>4</h3>
+                            </div>
+                        </Carousel>
+                    </Col>
+
+                    <List
+                        header={<div><h1 className="Price_card_heading"> Check Out Plan Here!</h1></div>}
+                        footer={<div style={{ textAlign: 'center' }}><button className="Heading_Button" style={{ width: "100%" }}>Continue(₹143)</button></div>}
+                        bordered
+                        dataSource={data1}
+                        style={{ width: windowDimensions.width > 720 ? "30%" : "100%" }}
+                        renderItem={item => (
+                            <List.Item>
+
+                                <CheckOutlined style={{ color: "darkorange", fontSize: "1rem" }} />  {item}
+                            </List.Item>
+                        )}
+                    />
+                </Row>
+
+                <Row>
+                    <h1 className="Content_Heading">Some More Freelancers</h1>
+                </Row>
+                <CardsRow />
+
+            </div>
         </Layout>
     )
 }
