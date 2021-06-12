736ab374d73516ffd22df8a77bf9b8b241982b2f
praduman99 <pradumanvats.000@gmail.com>
2021-06-13 00:39:32 +0530

    Todays commit

--- src/utility/Loader.js
+++ src/utility/Loader.js
@@ -4,8 +4,9 @@ import React from 'react'
 const Loader = (props) => {
     return (
         <div style={{color:"black",textAlign:"center"}}>
-            <Spin spinning={props.enable} style={{padding:"20rem"}} tip="Loading..." size="large" delay={5} >
-            </Spin>
+            {/* <Spin spinning={props.enable} style={{padding:"20rem"}} tip="Loading..." size="large" delay={5} >
+            </Spin> */}
+            
         </div>
     )
 }
