abd8cc742eb72ef6e67c4786e4a468b45e1c00d2
praduman99 <pradumanvats.000@gmail.com>
2021-06-17 22:39:23 +0530

    commit

--- src/Pages/Becomeaseller.js
+++ src/Pages/Becomeaseller.js
@@ -4,6 +4,126 @@ const Becomeaseller = () => {
     return (
         <div>
             s4fs4rfw4fs4rs4rsrsrs
+            {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
         </div>
     )
 }
--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -4,6 +4,114 @@ const Categories = () => {
     return (
         <div>
             ewtetgdgd4fsfs4fs4fs4fs4f4fs
+            {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
+             {/* oeirhfnismfsefsefsr
+            srg
+            sr
+            gsr
+            g
+            srg
+            sr
+            g
+            srg
+            sr
+            g
+             */}
         </div>
     )
 }
